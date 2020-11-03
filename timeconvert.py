0b849a463899b057b4afd4fac1460f0dfe6a93b0
AmalPrakash <pkamalprakash98@gmail.com>
2020-11-03 17:02:14 +0530

    Create timeconvert.py

---
+++ timeconvert.py
@@ -0,0 +1,3 @@
+#This code converts years to months, days, hours, minutes and seconds
+
+choice = input("Enter your choice :-\n1. Convert years to months days and hours\n2.Convert months to days hours and minutes\n3. Convert days to hours minutes and seconds\n4. Find hours, days, months and years between two dates\n")
